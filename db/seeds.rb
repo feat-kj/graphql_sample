6752e6d32f20a74490c1565a9a1b100c7dd02977
kenji ohata <kohhata@opendoor.co.jp>
2017-12-01 10:23:55 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-me = Person.create(first_name: 'Steven', last_name: 'Luscher', email: 'steveluscher@fb.com', username: 'steveluscher')
-dhh = Person.create(first_name: 'David', last_name: 'Heinemeier Hansson', email: 'dhh@37signals.com', username: 'dhh')
-ezra = Person.create(first_name: 'Ezra', last_name: 'Zygmuntowicz', email: 'ezra@merbivore.com', username: 'ezra')
-matz = Person.create(first_name: 'Yukihiro', last_name: 'Matsumoto', email: 'matz@heroku.com', username: 'matz')
-me.friends << [matz]
-dhh.friends << [ezra, matz]
-ezra.friends << [dhh, matz]
-matz.friends << [me, ezra, dhh]
+hanamichi = Person.create(first_name: 'Hanamichi', last_name: 'Sakuragi', email: 'hanamichi@shohoku.com', username: '退場王')
+minami    = Person.create(first_name: 'Tsuyoshi', last_name: 'Minami', email: 'minami@toyotama.com', username: 'カリメロ')
+fujima    = Person.create(first_name: 'Kenji', last_name: 'Fujima', email: 'fujima@shoyo.com', username: 'ホケツ')
+ikegami   = Person.create(first_name: 'Akira', last_name: 'Ikegami', email: 'ikegami@ryonan.com', username: '3年の池上')
+
+hanamichi.friends   << [minami]
+minami.friends  << [fujima]
+fujima.friends << [hanamichi, ikegami]
+ikegami.friends << [hanamichi, minami, fujima]
